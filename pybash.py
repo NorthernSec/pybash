fff931737734fba6f4b046990b6a373c2de06d51
Pieter-Jan Moreels <pieterjan.moreels@gmail.com>
2016-10-17 20:20:43 +0200

    sessions, alternative bash, functions and vars

--- pybash.py
+++ pybash.py
@@ -24,14 +24,17 @@ import sys
 import termios
 import traceback
 import tty
+import types
 
-# Only dependencey
+# Temporarily import this from an external library
+#  This will be implemented better, later on.
 from AdvancedInput import AdvancedInput
 
 # Constants
 REG_OTHER        = re.compile(",((?! ,).)* ,")
 REG_BASHVARS     = re.compile("^((?!\\\\).)?(\$\w+)|(\${\w+})")
 REG_PY_MULTILINE = re.compile("""^(def |if |\"\"\"((?!\"\"\").)*|\'\'\'((?!\'\'\').)*)""")
+NO_OUT = ['nano', 'vi', 'alsamixer', 'man']
 
 WIN = True if os.name == 'nt' else False
 
@@ -41,6 +44,9 @@ defaultSettings={
     "host": color.YELLOW,
     "path": color.CYAN,
     "text": color.END},
+  "autoload": False,
+  "autosave": False,
+  "session": None,
   "globs": {},
   "vars": {},
   "bash": True,
@@ -48,6 +54,19 @@ defaultSettings={
   "home": os.path.expanduser("~"),
   "history": []
 }
+
+HELP='''py or python               - Switch to python mode
+bash or sh                 - Switch to bash mode
+save [<session>]           - Save session
+load [<session>]           - Load session
+autosave <yes/no>          - Set session to autosave
+autoload <yes/no>          - Set session to autoload
+run <script>               - Run a script
+clear                      - Clear the terminal
+info or inspect <function> - Show source code of function
+settings                   - Print the settings
+help                       - Print this output
+exit                       - Exit pybash'''
 
 
 @contextlib.contextmanager
@@ -68,35 +87,55 @@ class pybash():
     self.settings["globs"]["_term"] = self
     self._input = AdvancedInput()
     self.settings["history"] = self._input.history
+    self.line = None
 
 
   def history(self, line=None, limit=50):
     if line:
-      return "Not implemented yet"
-    _h = []
-    for i, line in enumerate(self.settings["history"][-limit:]):
-      _h.append(" "+str(i)+" "*(5-len(str(i))) + " " + line)
-    return _h
+      try:
+        if int(line) <= len(self.settings["history"]):
+          self.line = self.settings["history"][int(line)]
+      except:
+          print("Please pass a line number")
+    else:
+      _h = []
+      for i, line in enumerate(self.settings["history"][-limit:]):
+        _h.append(" "+str(i)+" "*(5-len(str(i))) + " " + line)
+      print("\n".join(_h))
 
 
   def _saveSession(self, path=None):
-    if not path: path = os.path.join(self.settings["home"], ".pybash/session.pkl")
+    if path and self.settings['session'] and path != self.settings['session']:
+      print("The session path is different from the one loaded.")
+      print("Would you like to continue on the new copy?")
+      if AdvancedInput.confirm(True):
+        self.settings['session'] = path
+    if not path:
+      if self.settings['session']: path = self.settings['session']
+      else:                        path = os.path.join(self.settings["home"], ".pybash/session.pkl")
     if not os.path.exists(os.path.dirname(path)): os.makedirs(os.path.dirname(path))
     try:
+      self.settings.pop('session')
       pickle.dump(self.settings, open(path, "wb"))
-      print("Session saved! [%s]"%path)
+      self.settings['session'] = path
+      print("Session saved [%s]"%path)
     except Exception as e:
       print("Could not save session! (%s)"%e)
 
 
-  def _loadSession(self, path=None):
+  def _loadSession(self, path=None, auto=False):
     if not path: path = os.path.join(self.settings["home"], ".pybash/session.pkl")
     try:
       self.settings=pickle.load(open(path, "rb"))
       self._input.history = self.settings["history"]
-      print("Session loaded! [%s]"%path)
+      self.settings['session']=path
+      print("Session loaded [%s]"%path)
     except:
-      print("Could not load session: %s"%path)
+      if not auto: print("Could not load session: %s"%path)
+
+
+  def _clearSession(self):
+    self.settings = defaultSettings
 
 
   def _getCurs(self):
@@ -112,7 +151,9 @@ class pybash():
       path = os.getcwd()
       if path.startswith(self.settings["home"]):
         path = path.replace(self.settings["home"], "~", 1)
-      return "%s@%s:%s$ "%(color(user, "user"), color(host, "host"), 
+      if len(user+host+path)+3 > shutil.get_terminal_size().columns/2:
+        path = "->/"+os.path.basename(os.path.normpath(path))
+      return "%s@%s:%s$ "%(color(user, "user"), color(host, "host"),
                            color(path, "path"))
 
 
@@ -133,13 +174,15 @@ class pybash():
     _c, payload = command.split(" ", 1)+[""]*(2-len(command.split(" ", 1)))
     if   _c == "cd":              os.chdir(payload if payload else self.settings["home"])
     elif _c in ["clear", "cls"]:  os.system("cls" if WIN else "clear")
-    elif _c == "history":
-      if payload:  print("not implemented yet")
-      else:        print("\n".join(self.history()))
-    elif _c in ["nano", "vi"]:
-      print("You shouldn't run %s from pybash"%command.split()[0])
+    elif _c == "history":         self.history(payload)
+    elif _c in NO_OUT:
+      try:
+        subprocess.call([_c]+payload.split(" "))
+      except ProcessLookupError:
+        pass
+      #print("You shouldn't run %s from pybash"%command.split()[0])
     else:
-      return subprocess.Popen(command, shell=True, env=environment, 
+      return subprocess.Popen(command, shell=True, env=environment,
                               executable=self.settings["bash_binary"],
                               stdout=subprocess.PIPE).stdout.read().decode('utf-8')[:-1]
 
@@ -152,13 +195,28 @@ class pybash():
     elif _c in ["bash", "sh"]:      self.settings["bash"] = True
     elif _c in ["load"]:            self._loadSession(payload)
     elif _c in ["save"]:            self._saveSession(payload)
+    elif _c in ['autosave']:        self.settings["autosave"] = True if payload.lower() in ['yes', 'y', 'true', '1', ''] else False
+    elif _c in ['autoload']:        self.settings["autoload"] = True if payload.lower() in ['yes', 'y', 'true', '1', ''] else False
     elif _c in ["exit"]:            sys.exit()
     elif _c in ["clear"]:           os.system("cls" if WIN else "clear")
     elif _c in ["run"]:             self.runScript(payload)
-    elif _c in ["history"]:         print("\n".join(self.history()))
+    elif _c in ['help']:            print(HELP)
     elif _c in ["info", "inspect"]:
       funct=self.settings["vars"].get(payload.strip())
       print(funct.source if funct else "Unknown function")
+    elif _c in ['settings']:
+      for k,v in self.settings.items():
+        if k not in ['history', 'globs', 'vars']:
+          print("  %s: %s"%(k, repr(v)))
+    elif _c in ['vars', 'variables']:
+      for x, y in self.settings['vars'].items():
+        if type(y) is not types.FunctionType:
+          print("  %s  -  %s"%(x, repr(type(y)).split("'")[1]))
+    elif _c in ['functs', 'functions']:
+      for x, y in self.settings['vars'].items():
+        if type(y) is types.FunctionType:
+          vars = ", ".join(y.__code__.co_varnames)
+          print("  %s(%s)"%(x, vars))
     else:                          print("Command not known")
     return True
 
@@ -179,10 +237,13 @@ class pybash():
 
 
   def _interactiveShell(self):
+    self._loadSession(auto=True)
+    if not self.settings['autoload']: self._clearSession()
     multiline = None
     while True:
       try:
-        data = self._input.input("... " if multiline else self._getCurs())
+        data = self._input.input("... " if multiline else self._getCurs(), self.line)
+        self.line = None
         if not data and not multiline:
           pass
         if REG_PY_MULTILINE.match(data):
@@ -198,9 +259,10 @@ class pybash():
         data = self._parseCommand(data)
         if data: print(data)
       except KeyboardInterrupt:
-        print()
+        print("\nKeyboardInterrupt")
       except EOFError:
         print()
+        if self.settings['autosave']: self._saveSession()
         break
       except SystemExit:
         sys.exit()
@@ -238,7 +300,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description=description)
   parser.add_argument('file', metavar='file', nargs='?', help='Script to run')
   parser.add_argument('-p',   action='store_true',       help='Start script with python instead of bash')
-  
+
   args = parser.parse_args()
 
   _term = pybash(bash=(not args.p))
